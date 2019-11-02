887a5966b0ae85e6b4e7963e6191345c513a993c
Srikanth Reddy Nagidi <srikanth.nagidi@gmail.com>
2019-11-01 21:31:17 -0500

    added controller integration test

---
+++ pricing-service/src/test/java/com/udacity/pricing/web/PricingControllerIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.udacity.pricing.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class PricingControllerIntegrationTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void getPrice(){
+        ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("http://localhost:" + port + "/services/price?vehicleId=" + 2, String.class);
+
+        assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+}
