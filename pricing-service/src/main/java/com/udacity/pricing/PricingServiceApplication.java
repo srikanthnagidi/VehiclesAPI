0d7a772ed2ce69fbab3ee1958b3804a428e6e440
Srikanth Reddy Nagidi <srikanth.nagidi@gmail.com>
2019-11-06 15:46:55 -0600

    added Eureka server

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -3,6 +3,7 @@ package com.udacity.pricing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * TODO: Convert the application from a REST API to a microservice.
